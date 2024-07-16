6a83ba73dd4e8b8a04b4808c283d56e8a5e43ba4
rajeshpattnaik <rajeshpattanaik56280@gmail.com>
2024-07-17 02:14:25 +0530

    Connection Set-Up for Atlas and Model defined

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express, { application, Request, Response } from 'express';
+
+const app = express();
+
+const PORT:number = 3333;
+
+app.get("/",(req:Request,res:Response) =>{
+    res.send("hello world");
+})
+app.listen(PORT,() => {
+    console.log(`server is running on Port  ${PORT}`)
+})
